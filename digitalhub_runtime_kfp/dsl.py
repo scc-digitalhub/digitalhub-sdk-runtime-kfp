be08996e0610b97aaa112ee3d98dafa6e3fa2830
EvangMM <matteomartini92@yahoo.it>
2025-07-09 15:36:59 +0200

    refactor: refactor kfp

--- digitalhub_runtime_kfp/dsl.py
+++ digitalhub_runtime_kfp/dsl.py
@@ -38,6 +38,7 @@ class PipelineContext:
         action: str,
         function: str | None = None,
         workflow: str | None = None,
+        step_outputs: dict | None = None,
         **kwargs,
     ) -> dsl.ContainerOp:
         """
@@ -64,50 +65,38 @@ class PipelineContext:
         dsl.ContainerOp
             The constructed KFP ContainerOp.
         """
+
+        # Build command
+        cmd = ["python", "step.py"]
+
+        # Get project name
         project = os.environ.get(RuntimeEnvVar.PROJECT.value)
 
-        cmd = [
-            "python",
-            "step.py",
-            "--project",
-            project,
-            "--action",
-            action,
-        ]
+        # Add executable entity
+        try:
+            if function is not None:
+                exec_entity = dh.get_function(function, project=project)
+            elif workflow is not None:
+                exec_entity = dh.get_workflow(workflow, project=project)
+            else:
+                raise RuntimeError("Either function or workflow must be provided.")
+        except Exception as e:
+            raise RuntimeError("Function or workflow not found.") from e
+
+        cmd += ["--entity", exec_entity.key]
 
         # Prepare execution kwargs
         exec_kwargs = {k: v for k, v in {**kwargs}.items() if v is not None}
+        exec_kwargs["action"] = action
+        cmd += ["--kwargs", json.dumps(exec_kwargs, cls=PipelineParamEncoder)]
 
         # Prepare outputs
         file_outputs = {"run_id": "/tmp/run_id"}
-        if "outputs" in exec_kwargs:
-            if isinstance(exec_kwargs["outputs"], dict):
-                for val in exec_kwargs["outputs"].values():
-                    oname = str(val).replace(".", "_")
-                    file_outputs[oname] = f"/tmp/entity_{oname}"
-
-        cmd += ["--execkwargs", json.dumps(exec_kwargs, cls=PipelineParamEncoder)]
-
-        # Add function or workflow
-        if function is None and workflow is None:
-            raise RuntimeError("Either function or workflow must be provided.")
-
-        exec_entity = None
-        if function is not None:
-            try:
-                exec_entity = dh.get_function(function, project=project)
-            except Exception:
-                raise RuntimeError(f"Function {function} not found")
-            cmd += ["--function", function, "--function_id", exec_entity.id]
-        elif workflow is not None:
-            try:
-                exec_entity = dh.get_workflow(workflow, project=project)
-            except Exception:
-                raise RuntimeError(f"Workflow {workflow} not found")
-            cmd += ["--workflow", workflow, "--workflow_id", exec_entity.id]
-
-        if exec_entity is None:
-            raise RuntimeError("Function or workflow not found")
+        if step_outputs is not None:
+            for val in step_outputs.values():
+                # Sanitize . in output names
+                oname = str(val).replace(".", "_")
+                file_outputs[oname] = f"/tmp/entity_{oname}"
 
         # Create ContainerOp
         cop = dsl.ContainerOp(
